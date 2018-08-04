dfa383b143d977ca0e6b6c51737e33ae0dcccee3
jvhoffbauer <jv@lewe-hoffbauer.de>
2018-08-04 10:46:51 +0200

    Cleanup, add config options and logging

--- viz3d/config.py
+++ viz3d/config.py
@@ -1,6 +1,24 @@
 import json
+import logging
 
+_config = None
 
 def read_config():
-    with open("../config.json") as f:
-        return json.load(f)
+    """
+    :return: Project specific config
+    """
+
+    global _config
+
+    # Check if config was already loaded
+    if _config is not None:
+        return _config
+
+    with open("config.json") as f:
+        # Load config
+        config = json.load(f)
+        # Parse
+        config["log"]["level"] = logging.getLevelName(config["log"]["level"])
+        # Store config
+        _config = config
+        return _config
--- viz3d/image.py
+++ viz3d/image.py
@@ -1,40 +1,52 @@
 import cv2
-import numpy as np
-import config
 import time
+import os.path
+import config
+import logging
+
+# Load config
+cfg = config.read_config()
+
+# Create logger
+logger = logging.getLogger("image")
+logging.basicConfig(format=cfg["log"]["format"], level=cfg["log"]["level"])
+
 
 if __name__ == "__main__":
-    cfg = config.read_config()
 
+    # Left and right from camera view
     left = cv2.VideoCapture(cfg["cameras"]["left"])
     right = cv2.VideoCapture(cfg["cameras"]["right"])
 
+    # Set camera fps from config
     left.set(cv2.CAP_PROP_FPS, cfg["cameras"]["fps"])
     right.set(cv2.CAP_PROP_FPS, cfg["cameras"]["fps"])
 
     while True:
+        # Get images from both cameras
         ret, frameLeft = left.read()
         if not ret:
-            print("Cannot load left camera!")
+            logger.error("Cannot load left camera!")
             break
         ret, frameRight = right.read()
         if not ret:
-            print("Cannot load right camera!")
+            logger.error("Cannot load right camera!")
             break
-
+        # Convert to grayscale images
         grayLeft = cv2.cvtColor(frameLeft, cv2.COLOR_BGR2GRAY)
         grayRight = cv2.cvtColor(frameRight, cv2.COLOR_BGR2GRAY)
-
+        # Show images
         cv2.imshow('frameLeft', grayLeft)
         cv2.imshow('frameRight', grayRight)
+        # Check for key input. Close on 'q', capture image on 'c'
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
         elif key == ord('c'):
             epoch = int(time.time())
-            cv2.imwrite("../captures/%i-left.png" % epoch, grayLeft)
-            cv2.imwrite("../captures/%i-right.png" % epoch, grayRight)
+            ret1 = cv2.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-left.png" % epoch), grayLeft)
+            ret2 = cv2.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-right.png" % epoch), grayRight)
+            logger.info("Captured images. Left=%r. Right=%r" % (ret1, ret2))
 
     left.release()
     right.release()
-    cv2.destroyAllWindows()
