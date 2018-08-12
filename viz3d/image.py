d9a2be13ac2936c4c7eb9115db1b2f8e6280d4a8
jvhoffbauer <jv@lewe-hoffbauer.de>
2018-08-12 10:54:23 +0200

    Make image.py write color (BGR) images

--- viz3d/image.py
+++ viz3d/image.py
@@ -4,6 +4,7 @@ import os.path
 import config
 import logging
 import calibration
+import numpy as np
 
 # Load config
 cfg = config.read_config()
@@ -33,23 +34,30 @@ if __name__ == "__main__":
         if not ret:
             logger.error("Cannot load right camera!")
             break
+
         # Convert to grayscale images
         grayLeft = cv.cvtColor(frameLeft, cv.COLOR_BGR2GRAY)
         grayRight = cv.cvtColor(frameRight, cv.COLOR_BGR2GRAY)
+
+        imageMarkerLeft = np.copy(frameLeft)
+        imageMarkerRight = np.copy(frameRight)
+
         # Mark checkerboard corners
-        calibration.mark_checkerboard_corners(grayLeft, imageMarker=frameLeft)
-        calibration.mark_checkerboard_corners(grayRight, imageMarker=frameRight)
+        calibration.mark_checkerboard_corners(grayLeft, imageMarker=imageMarkerLeft)
+        calibration.mark_checkerboard_corners(grayRight, imageMarker=imageMarkerRight)
+
         # Show images
-        cv.imshow('frameLeft', frameLeft)
-        cv.imshow('frameRight', frameRight)
+        cv.imshow('frameLeft', imageMarkerLeft)
+        cv.imshow('frameRight', imageMarkerRight)
+
         # Check for key input. Close on 'q', capture image on 'c'
         key = cv.waitKey(1) & 0xFF
         if key == ord('q'):
             break
         elif key == ord('c'):
             epoch = int(time.time() * 1000)
-            ret1 = cv.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-left.png" % epoch), grayLeft)
-            ret2 = cv.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-right.png" % epoch), grayRight)
+            ret1 = cv.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-left.png" % epoch), frameLeft)
+            ret2 = cv.imwrite(os.path.join(cfg["workingDir"], "captures", "%i-right.png" % epoch), frameRight)
             logger.info("Captured images. Left=%r. Right=%r" % (ret1, ret2))
 
     left.release()
