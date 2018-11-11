131bb131ab632490c96e5f9e03eb33ade86e1909
Turakar <turakar23@gmail.com>
2018-11-11 16:22:31 +0100

    Refactor block-matching
    - object orientation for disparity calculators
    - more flexible disparity calculation

--- viz3d/block_matching.py
+++ viz3d/block_matching.py
@@ -13,53 +13,38 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)s : %(name)s : %(message)s", level=logging.INFO)
 
 
+NO_DISPARITY_VALUE = 255
+
+
 class BlockMatcher:
-    def __init__(self, cost_function, window_size):
+    def __init__(self, disparity_calculator, window_size):
         assert window_size % 2 == 1
-        self.cost_function = cost_function
+        self.disparity_calculator = disparity_calculator
         self.window_size = window_size
 
     def calculate_disparity_map(self, image_left, image_right):
         # Check preconditions
         assert image_left.shape == image_right.shape
 
-        # Sample windows from both images
-        window_shape = (self.window_size, self.window_size)
-        windows_left = view_as_windows(image_left, window_shape)
-        windows_right = view_as_windows(image_right, window_shape)
-        windows_x_count = windows_left.shape[1]
-        windows_y_count = windows_left.shape[0]
+        # Calculate windows borders
+        image_width = image_left.shape[1]
+        image_height = image_left.shape[0]
+        windows_x_count = image_width - self.window_size + 1
+        windows_y_count = image_height - self.window_size + 1
+
+        # Prepare disparity calculation
+        self.disparity_calculator.prepare(image_left, image_right, self.window_size)
 
         # Create empty disparity map
         disparity_map = np.zeros([windows_y_count, windows_x_count], dtype=np.int32)
-
-        # DEBUG costs stats
-        costs_all = np.zeros([windows_y_count, windows_x_count, windows_x_count], dtype=np.float64)
 
         # Iterate all windows of the left image
         for y in range(windows_y_count):
             for x in range(windows_x_count):
 
-                # Retrieve relevant windows
-                window_left = windows_left[y, x]
-                windows_right_row = windows_right[y, :]
-
-                # Create evaluation batch for epipolar line
-                windows_left_row = self.create_row(window_left, windows_right_row)
-
-                # Calculate costs
-                costs = self.cost_function(windows_left_row, windows_right_row)
-                costs_all[y, x] = costs
-
-                # Get best match
-                match = np.argmin(costs)
-                if costs[match] == np.finfo(costs.dtype).max:
-                    match = -1
-
-                # Calculate disparity
-                if match != -1:
-                    disparity = x - match
-                    disparity_map[y, x] = disparity
+                # Calculate disparities
+                disparity = self.disparity_calculator.calculate_disparity(y, x)
+                disparity_map[y, x] = disparity
 
             # Log progress
             logger.info("Processed %i of %i rows." % (y + 1, windows_y_count))
@@ -68,31 +53,104 @@ class BlockMatcher:
         padding = int(self.window_size / 2)
         disparity_map = np.pad(disparity_map, padding, "constant", constant_values=0)
 
-        return disparity_map, costs_all
+        return disparity_map
 
-    def create_row(self, window_left, windows_right_row):
+
+class DisparityCalculator:
+
+    def prepare(self, image_left, image_right, window_size):
+        """
+        Prepares calculation of the costs for a single image pair
+        :param image_left:
+        :param image_right:
+        :param window_size:
+        """
+        raise NotImplementedError()
+
+    def calculate_disparity(self, y_window, left_x_window):
+        """
+        Calculates the disparity for this window
+        :param y_window: y coordinate of the window
+        :param left_x_window: x coordinate of the window, only valid for the left windows
+        :return: the calculated disparity or NO_DISPARITY_VALUE if no match was found
+        """
+        raise NotImplementedError()
+
+
+class DisparityWindowed(DisparityCalculator):
+
+    def __init__(self, max_disparity):
+        self.window_size = None
+        self.windows_left = None
+        self.windows_right = None
+        self.max_disparity = max_disparity
+
+    def prepare(self, image_left, image_right, window_size):
+        window_shape = (window_size, window_size)
+        self.window_size = window_size
+        self.windows_left = view_as_windows(image_left, window_shape)
+        self.windows_right = view_as_windows(image_right, window_shape)
+
+    def calculate_disparity(self, y_window, left_x_window):
+        # Get right window row
+        # Calculate row extending max_disparity to the left and right, but not out of bounds
+        row_start = max(0, left_x_window - self.max_disparity)  # Do not extend to negative values
+        row_end = min(self.windows_right.shape[1], left_x_window + self.max_disparity)  # Do not extend over the windows count
+        # Get row
+        windows_right_row = self.windows_right[y_window, row_start:row_end]
+
+        # Get left window and duplicate it in a row
+        window_left = self.windows_left[y_window, left_x_window]
         windows_row_count = windows_right_row.shape[0]
         windows_left_row = np.zeros([windows_row_count, self.window_size, self.window_size], dtype=window_left.dtype)
         windows_left_row[...] = window_left
-        return windows_left_row
+
+        # Call subclass' cost
+        costs = self.cost_windowed(windows_left_row, windows_right_row)
+
+        # Get best match
+        match = np.argmin(costs)
+        if costs[match] == np.finfo(costs.dtype).max:
+            match = -1
+
+        # Calculate disparity
+        disparity = NO_DISPARITY_VALUE
+        if match != -1:
+            right_x = row_start + match
+            disparity = left_x_window - right_x
+
+        if disparity < 0:
+            disparity = NO_DISPARITY_VALUE
+
+        return disparity
+
+    def cost_windowed(self, windows_left_row, windows_right_row):
+        raise NotImplementedError("Has to be implemented in subclass!")
 
 
-def cost_mse(windows_left, windows_right):
-    mse = ((windows_left - windows_right) ** 2).mean(axis=(1, 2))
-    mse[mse > 6] = np.finfo(mse.dtype).max
-    return mse
+class DisparityMse(DisparityWindowed):
+
+    def __init__(self, max_disparity, threshold):
+        super().__init__(max_disparity)
+        self.threshold = threshold
+
+    def cost_windowed(self, windows_left_row, windows_right_row):
+        mse = ((windows_left_row - windows_right_row) ** 2).mean(axis=(1, 2))
+        mse[mse > self.threshold] = np.finfo(mse.dtype).max
+        return mse
 
 
-def create_cost_cnn(path):
-    model = load_model(path)
+class DisparityCnn(DisparityWindowed):
 
-    def cost_cnn(windows_left, windows_right):
-        classes = model.predict([windows_left[..., np.newaxis], windows_right[..., np.newaxis]])
+    def __init__(self, max_disparity, model_path):
+        super().__init__(max_disparity)
+        self.model = load_model(model_path)
+
+    def cost_windowed(self, windows_left, windows_right):
+        classes = self.model.predict([windows_left[..., np.newaxis], windows_right[..., np.newaxis]])
         costs = 1 - classes[:, 1]
         costs[classes[:, 0] > classes[:, 1]] = 1
         return costs
-
-    return cost_cnn
 
 
 def main():
@@ -101,8 +159,10 @@ def main():
     image_right = cv.imread(join(path, "colored_1", "000001_10.png"), cv.IMREAD_GRAYSCALE)
     disparity_map_correct = cv.imread(join(path, "disp_noc", "000001_10.png"), cv.IMREAD_GRAYSCALE)
 
-    block_matcher = BlockMatcher(create_cost_cnn("models/experimental_cnn/model.h5"), 9)
-    disparity_map, costs_all = block_matcher.calculate_disparity_map(image_left, image_right)
+    #disparity_calculator = DisparityCnn("models/experimental_cnn/model.h5")
+    disparity_calculator = DisparityMse(100, 10000000)
+    block_matcher = BlockMatcher(disparity_calculator, 9)
+    disparity_map = block_matcher.calculate_disparity_map(image_left, image_right)
 
     #disparity_map_correct_flat = disparity_map_correct[4:-4, 4:-4].reshape(-1)
     #costs_all_flat = costs_all.min(axis=2).reshape(-1)
